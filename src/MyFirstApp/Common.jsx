2c4743c352da2ebb5fa022718a003d203f0021a6
akashkush7 <akashkushwaha060@gmail.com>
2024-01-12 12:50:46 +0530

    Home page Upgrade

--- src/MyFirstApp/Common.jsx
+++ src/MyFirstApp/Common.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import CommonImg from "../Images/Home.png";
 
 const Common = (props) => {
     return (
-        <div className="bgimg">
-            <div className="center text-center">
+        <div className="HomeC">
+            <div className='imagediv'>
+                <img src={CommonImg} alt="Home.png" />
+            </div>
+            <div className="center text-center w-75 mt-5">
                 <h1 className="fw-bold text-capitalize">{props.name}</h1>
                 <br />
                 <p>{props.about}</p>
                 <br />
-                <NavLink className="btn btn-light fs-4" to={props.url}>{props.btnname}</NavLink>
+                <NavLink className="btn btn-dark fs-4 mb-5" to={props.url}>{props.btnname}</NavLink>
             </div>
         </div>
     );
--- src/MyFirstApp/NavBar.jsx
+++ src/MyFirstApp/NavBar.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import "./NavBar.css";
+import Logo from "../Images/favicon.ico";
 
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-black fixed-top">
       <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1">One-For-All</span>
+        <div>
+          <img src={Logo} alt="SiteLogo" height="30px" />
+          <span className="navbar-brand mb-0 h1" style={{ marginLeft: "5px" }}>One-For-All</span>
+        </div>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
